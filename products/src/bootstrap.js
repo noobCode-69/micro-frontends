64dbda0f835afffc1ba62842c8c990e906f29c55
so_hell <armanyppph@gmail.com>
2023-05-02 05:04:26 +0530

    finished

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,33 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
+  for (let i = 0; i < 5; ++i) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+  el.innerHTML = products;
+};
 
-for (let i = 0; i < 5; ++i) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+// Context/Situation #1
+// We are running this file in the developemnt in isolation
+// We are using our local index.html file
+// Which DEFNITELY has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+  // Assuming out container doesn't have an element
+  // with id 'dev-products' ...
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+// Context/Siutation #2
+// We are running this file in development or production
+// through the CONTAINER app
+// No GURANTEE that an element with and id of 'dev-products'
+// WE DON NOT WATN try to immediately render the app
+
+export { mount };
